772360cbc9db5aca507d7b313ee45f61be46e15d
Jhonathan Feliz <sakurakojisakura008@gmail.com>
2025-07-30 17:55:19 -0400

    Microservicio de products casi listo falta controllers y router mas app.js para que funcione

--- products/controller/product.js
+++ products/controller/product.js
@@ -8,7 +8,18 @@ export const getProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export const getProductById = async (req, res) => {};
+export const getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Products.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 export const createProduct = async (req, res) => {};
 export const updateProduct = async (req, res) => {};
 export const deleteProduct = async (req, res) => {};
